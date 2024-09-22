13b61cad8bd35532d19f371e5b339ee50cc732f3
MegaDog666 <Moonandre9999@mail.ru>
2024-09-23 00:42:53 +0300

    22.09.2024-23.09.2024

---
+++ src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution() {
+
+    }
+
+    private Solution(String s) {
+
+    }
+
+    protected Solution(int i) {
+
+    }
+
+    Solution(double d) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task15.task1525;
+
+import java.nio.file.Path;
+
+public class Statics {
+    public static String FILE_NAME = "/Users/andrey./javarush/3443774/javarush-project/src/com/javarush/task/task15/task1525/TEST";
+}
